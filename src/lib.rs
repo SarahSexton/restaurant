4de695956e94fcce39b0356cc0b3a2b693453180
Sarah <sarahjeannesexton@live.com>
2024-10-15 14:37:53 -0700

    init commit

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
+
+mod front_of_house;
+
+pub use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
+}
